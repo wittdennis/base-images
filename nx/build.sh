f8b5789f5b5e8d08d638f12418b6d09c83de440a
Dennis Witt <witt.dennis@outlook.com>
2023-08-25 16:02:13 +0200

    wip nx build script

--- nx/build.sh
+++ nx/build.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+function join {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
@@ -23,10 +30,11 @@ TAG="$NX_VERSION-$(echo $NODE_IMAGE | sed 's|:|-|g')"
 MANIFEST="$REPOSITORY:$TAG"
 PLATFORMS=("linux/amd64")
 
-$CONTAINER_CMD manifest create $MANIFEST 2> /dev/null || true
+IMAGES=()
 for PLATFORM in ${PLATFORMS[@]}; do    
     PLATFORM_TAG=$(echo $PLATFORM | sed 's|/|-|g')
     IMAGE="$REPOSITORY:$TAG-$PLATFORM_TAG"
+    IMAGES+=($IMAGE)
     echo "Building image for platform $PLATFORM using platform tag: $PLATFORM_TAG"
 
     $CONTAINER_CMD build \
@@ -36,7 +44,15 @@ for PLATFORM in ${PLATFORMS[@]}; do
             --build-arg NODE_IMAGE=$NODE_IMAGE \
             --platform "$PLATFORM" -t "$IMAGE" -f "$SCRIPT_PATH/Dockerfile" "$SCRIPT_PATH"    
     $CONTAINER_CMD push "$IMAGE"
-    $CONTAINER_CMD manifest add "$MANIFEST" docker://$IMAGE
 done
 
-$CONTAINER_CMD manifest push --all "$MANIFEST"
+MANIFEST_AMENDS=$(join ' --amends ' ${IMAGES[@]})
+$CONTAINER_CMD manifest rm "$MANIFEST" 2> /dev/null || true
+$CONTAINER_CMD manifest create "$MANIFEST" --amend $MANIFEST_AMENDS
+
+if [[ $CONTAINER_CMD == *"podman"* ]]; then
+    # this is only needed because the github runner ubuntu-22.04 does not have a podman version with PR: https://github.com/containers/podman/pull/18395
+    $CONTAINER_CMD manifest push "$MANIFEST" "docker://$MANIFEST"
+else
+    $CONTAINER_CMD manifest push "$MANIFEST"
+fi
